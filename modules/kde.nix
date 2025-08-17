981e2d166d2afcbfa748c9ec7dda695b24b4c5dd
izen67 <17254913+izen67@users.noreply.github.com>
2025-08-17 12:55:12 +0300

    commit

---
+++ modules/kde.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  environment.variables.QT_QPA_PLATFORMTHEME = "qt5ct";
+
+  environment.systemPackages = with pkgs; [
+    papirus-icon-theme
+    kdePackages.sddm-kcm
+    libunity
+    kdePackages.kate
+    libsForQt5.qtstyleplugin-kvantum
+    kdePackages.qtstyleplugin-kvantum
+    kdePackages.plasma-firewall
+    plasma-panel-colorizer
+  ];
+}
