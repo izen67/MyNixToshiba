98d5587f0eb490c0fe35a96f813085d71ea80ac1
izen67 <17254913+izen67@users.noreply.github.com>
2025-08-24 17:05:00 +0300

    Add files via upload

---
+++ modules/bluetooth.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+        General = {
+            Experimental = true; # Show battery charge of Bluetooth devices
+            Enable = "Source,Sink,Media,Socket"; # May be unnecessary
+            };
+        };
+    };
+
+systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = [ "network.target" "sound.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+    };
+}
